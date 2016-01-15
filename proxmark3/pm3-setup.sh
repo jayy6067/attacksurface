c1a2bc628d5284d1a01b81e7daa47e68cf8106ee
Dave Williams <dave@dave.io>
2016-01-15 02:38:42 +0000

    add issues link to pm3 setup script

--- proxmark3/pm3-setup.sh
+++ proxmark3/pm3-setup.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 # Proxmark3 setup script
 # https://attacksurface.io/go/pm3
+# Report bugs at https://github.com/daveio/attacksurface/issues
 
 if grep -q "Ubuntu 14.04" /etc/issue; then
 	echo "Ubuntu 14.04 detected, continuing"
